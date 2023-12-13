33066f65496bf971abef6af9a364b6243f484937
VIJEET DELIWALA <=>
2023-12-13 23:30:45 +0530

    Importing Data

---
+++ pandas_import.py
@@ -0,0 +1,14 @@
+# Import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+
+data = pd.read_csv(file,nrows=5,header=None)
+# Build a numpy array from the DataFrame: data_array
+data_array = np.array(data)
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
